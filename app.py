5771b4bd08d3f39cffe4c11722d931886f06f9a4
andresito23 <56003216+andresito23@users.noreply.github.com>
2026-09-19 11:28:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import libreria_funciones as lf
+
 st.title("Paradigmas de la Programación")
 
 st.sidebar.image("logocsg.png")
@@ -7,3 +8,8 @@ st.sidebar.image("logocsg.png")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por Andrés Alvear")
+
+capital = st.number.input ("ingrese el Capital")
+tasa_anualpct = st.number_input ("Ingrese la tasa anual")
+
+resultado = lf.calcular_interes_mora()
