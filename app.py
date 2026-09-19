f3b900189b8bb86d81bad0dd869f7e3454f658c2
andresito23 <56003216+andresito23@users.noreply.github.com>
2026-09-19 11:56:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por Andrés Alvear")
 
-capital = st.number_input("ingrese el Capital")
-tasa_anualpct = st.number_input("Ingrese la tasa anual")
-dia_mora = st.number_input("Ingrese los días de mora")
+capital = st.number_input("Ingrese el capital", value = 1000)
+tasa_anual_pct = st.number_input("Ingrese de la tasa anual", value = 0.15)
+dias_mora = st.number_input("Ingrese los días de mora",value =30)
 
-#resultado = lf.calcular_interes_mora()
+resultado = lf.calcular_interes_mora(capital,tasa_anual_pct,dias_mora)
+
+st.write("El reusltado por atraso es:", resultado)
